b9a743b1bed13394bd8245996744e0d9ebea530a
volodya-lombrozo <volodya.lombrozo@yandex.ru>
2021-10-03 23:27:03 +0300

    chore: create prometheus.server.host property

--- src/main/java/com/github/volodya_lombrozo/scout/ScoutApplication.java
+++ src/main/java/com/github/volodya_lombrozo/scout/ScoutApplication.java
@@ -1,6 +1,8 @@
 package com.github.volodya_lombrozo.scout;
 
 import com.github.volodya_lombrozo.scout.prometheus.PrometheusScout;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScoutApplication implements CommandLineRunner {
 
-    private final Scout scout = new PrometheusScout();
+    private final Scout scout;
+
+    @Autowired
+    public ScoutApplication(@Value("${prometheus.server.host}") String prometheusHost) {
+        scout = new PrometheusScout(prometheusHost);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ScoutApplication.class);
